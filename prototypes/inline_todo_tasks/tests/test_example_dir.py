f26f8ba5b18db31d80484b67e11caede3e1864ac
Kale Miller <kale.miller96@gmail.com>
2023-10-27 19:25:43 +1100

    Added first set of puzzle finder tests

--- prototypes/inline_todo_tasks/tests/test_example_dir.py
+++
@@ -1,7 +0,0 @@
-"""Validates we get the expected results when running against the example directory."""
-
-import puzzles
-
-
-def test_against_example_dir():
-    assert False
---
+++ prototypes/inline_todo_tasks/tests/test_puzzle_finder.py
@@ -0,0 +1,52 @@
+"""Validates the puzzle finder works as expected."""
+
+from puzzles.algorithms import PuzzleFinder
+
+
+def test_can_find_single_line_comment_puzzle():
+    """Validates the puzzle finder can find a single line comment puzzle."""
+
+    puzzles = PuzzleFinder().find(
+        """
+        # [TODO 1] This is a single line comment puzzle.
+        """
+    )
+
+    assert len(puzzles) == 1
+
+
+def test_can_find_single_line_docstring_puzzle():
+    """Validates the puzzle finder can find a single line docstring puzzle."""
+
+    puzzles = PuzzleFinder().find(
+        '''
+        """[TODO 1] This is a single line docstring puzzle."""
+        '''
+    )
+
+    assert len(puzzles) == 1
+
+
+def test_can_find_inline_puzzle():
+    """Validates the puzzle finder can find an inline puzzle."""
+
+    puzzles = PuzzleFinder().find(
+        """
+        a = f(b, c)  # [TODO 1] This is a single line comment puzzle.
+        """
+    )
+
+    assert len(puzzles) == 1
+
+
+def test_can_find_multiple_puzzles():
+    """Validates the puzzle finder can find multiple puzzles."""
+
+    puzzles = PuzzleFinder().find(
+        """
+        # [TODO 1] This is a single line comment puzzle.
+        # [TODO 2] This is a single line comment puzzle.
+        """
+    )
+
+    assert len(puzzles) == 2
