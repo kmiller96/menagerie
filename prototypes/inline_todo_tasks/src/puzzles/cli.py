42dc8626d7d2e77606a1287c04026329a2caca9e
Kale Miller <kale.miller96@gmail.com>
2023-10-27 19:16:17 +1100

    Added the ability to export to different formats

--- prototypes/inline_todo_tasks/src/puzzles/cli.py
+++ prototypes/inline_todo_tasks/src/puzzles/cli.py
@@ -13,9 +13,10 @@ def cli():
 
 
 @cli.command()
+@click.option("-f", "--format", type=click.Choice(["todo", "json"]), default="todo")
 @click.argument("outpath", type=click.Path(file_okay=True, dir_okay=False))
 @click.argument("path", type=click.Path(exists=True))
-def run(outpath: str, path: str):
+def run(format: str, outpath: str, path: str):
     """Extracts all puzzles from the supplied path."""
     outpath: Path = Path(outpath)
     path: Path = Path(path)
@@ -23,4 +24,9 @@ def run(outpath: str, path: str):
     puzzles = main.search(path)
     fmt = formatter.Formatter(puzzles)
 
-    outpath.write_text(fmt.to_todo(), encoding="utf-8")
+    if format == "json":
+        outpath.write_text(fmt.to_json(), encoding="utf-8")
+    elif format == "todo":
+        outpath.write_text(fmt.to_todo(), encoding="utf-8")
+    else:
+        raise ValueError(f"Unknown format: {format}")
