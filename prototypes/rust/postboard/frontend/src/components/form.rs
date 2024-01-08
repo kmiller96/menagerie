b87e1918552cca28a18a61a138c2a75c99c78fd0
Kale Miller <kale.miller96@gmail.com>
2024-01-08 12:56:59 +0800

    Setup a simple form POST

--- prototypes/rust/postboard/frontend/src/components/form.rs
+++ prototypes/rust/postboard/frontend/src/components/form.rs
@@ -3,6 +3,9 @@ use yew::prelude::*;
 #[function_component]
 pub fn PostForm() -> Html {
     html! {
-        <p>{ "Post Form!" } </p>
+        <form method="POST" action="http://localhost:8000/api/post">
+            <input type="textarea" id="content" />
+            <input type="submit" value="Submit" id="submit" />
+        </form>
     }
 }
