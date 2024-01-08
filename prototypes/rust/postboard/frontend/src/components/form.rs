61376d531fd4c2dbbad916e4f2f6b44bf3d7223c
Kale Miller <kale.miller96@gmail.com>
2024-01-08 19:29:13 +0800

    Compiling, but it's weird...

--- prototypes/rust/postboard/frontend/src/components/form.rs
+++ prototypes/rust/postboard/frontend/src/components/form.rs
@@ -1,10 +1,28 @@
 use yew::prelude::*;
 
+use gloo::console::log;
+
 #[function_component]
 pub fn PostForm() -> Html {
+    let content = use_state(|| String::from(""));
+
+    let on_input = {
+        let content = content.clone();
+        Callback::from(move |e: InputEvent| content.set(e.data().unwrap_or_default()))
+    };
+
+    let on_submit = {
+        let content = content.clone();
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+            log!(content.get(0..)) // HACK: I have no idea why I need to do this...
+        })
+    };
+
     html! {
-        <form method="POST" action="http://localhost:8000/api/post">
-            <input type="textarea" id="content" />
+        <form onsubmit={on_submit}>
+            <input type="textarea" id="content" oninput={on_input} />
+            <br />
             <input type="submit" value="Submit" id="submit" />
         </form>
     }
