1a0dfd19c98f46208de548d6aa8e6356a4619f55
Kale Miller <kale.miller96@gmail.com>
2024-01-09 11:17:26 +0800

    This sucks; moving to a static site

--- prototypes/rust/postboard/frontend/src/components/form.rs
+++ prototypes/rust/postboard/frontend/src/components/form.rs
@@ -1,27 +1,25 @@
 use yew::prelude::*;
 
-use gloo::console::log;
+use gloo::{console::log, utils::document};
 
 #[function_component]
 pub fn PostForm() -> Html {
     let content = use_state(|| String::from(""));
 
-    let on_input = {
-        let content = content.clone();
-        Callback::from(move |e: InputEvent| content.set(e.data().unwrap_or_default()))
-    };
+    let onchange = Callback::from(|e: Event| {
+        let target = e.target();
+        log!(target);
+    });
 
-    let on_submit = {
-        let content = content.clone();
+    let onsubmit = {
         Callback::from(move |e: SubmitEvent| {
             e.prevent_default();
-            log!(content.get(0..)) // HACK: I have no idea why I need to do this...
         })
     };
 
     html! {
-        <form onsubmit={on_submit}>
-            <input type="textarea" id="content" oninput={on_input} />
+        <form {onsubmit}>
+            <input type="textarea" id="content" {onchange} />
             <br />
             <input type="submit" value="Submit" id="submit" />
         </form>
