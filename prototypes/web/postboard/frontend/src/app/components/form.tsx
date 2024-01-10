9a6dbd8034d3cc12da8ea2f7b28be92c826730ca
Kale Miller <kale.miller96@gmail.com>
2024-01-10 16:18:15 +0800

    Posted data to the backend

--- prototypes/web/postboard/frontend/src/app/components/form.tsx
+++ prototypes/web/postboard/frontend/src/app/components/form.tsx
@@ -1,19 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
 import styles from "./form.module.css";
 
 /** Defines the submission form */
 export default function SubmissionForm() {
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    fetch("/api/post", {
+      method: "POST",
+      body: JSON.stringify({ author, content }),
+    })
+      .then((response) => response.json())
+      .then((response) => console.log(JSON.stringify(response)));
+  };
+
   return (
     <div className={styles.form}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <textarea
             id="content"
             autoCorrect="on"
             placeholder="What would you like to say?"
+            required
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+              setContent(e.target.value);
+            }}
           />
         </div>
         <div>
-          <input type="text" id="author" placeholder="Name (Optional)" />
+          <input
+            type="text"
+            id="author"
+            placeholder="Name (Optional)"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setAuthor(e.target.value);
+            }}
+          />
         </div>
         <div>
           <input type="submit" id="submit" />
