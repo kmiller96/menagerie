b012201a0770e4ef755d7dcb1568a86a89570d9d
Kale Miller <kale.miller96@gmail.com>
2024-04-03 13:38:35 +0800

    Protected the endpoint

--- prototypes/web/fastapi_google_sso/server.py
+++ prototypes/web/fastapi_google_sso/server.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
 
 @app.get("/")
 def index():
     return {"hello": "world"}
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
