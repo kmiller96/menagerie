3a8cde8150d97bc4ebd7a1b26d375cbffe094025
Kale Miller <kale.miller@prometheusai.com.au>
2024-06-14 17:40:50 +0200

    More refactoring to make my life easier

--- prototypes/games/hearts/src/hearts/main.py
+++ prototypes/games/hearts/src/hearts/main.py
@@ -4,50 +4,62 @@ from hearts.pile import Pile
 from hearts.players import Player
 from hearts.logic import create_deck, determine_starting_player, winning_card
 
+##################
+## Main Routine ##
+##################
+
 
 def play(player_names: list[str]):
     """Runs the main game loop."""
 
-    # -- Initialise the players -- #
     num_players = len(player_names)
 
-    # -- Initialise the deck -- #
+    # -- Initialise the game -- #
     deck = create_deck()
-    hands = deck.split(num_players)
-
-    # -- Initialise the players -- #
-    players = [Player(name, hand=hand) for name, hand in zip(player_names, hands)]
+    players = [
+        Player(name, hand=hand)
+        for name, hand in zip(player_names, deck.split(num_players))
+    ]
 
     # -- Main game loop -- #
     leader = determine_starting_player(players)
     trick_number = 0
 
     while trick_number < 13:
-        print("Starting trick: ", trick_number)
-        trick = Pile()
-
-        # -- Play the trick -- #
-        start = players.index(leader)
-        play_order = [players[(start + i) % num_players] for i in range(num_players)]
-
-        for current_player in play_order:
-            card = current_player.play()
-            trick.add(card)
-
-        # -- Determine the winner -- #
-        winner = play_order[trick.cards.index(winning_card(trick))]
-        winner.tricks.append(trick)
+        winner = _play_trick(players, leader)
 
         leader = winner
         trick_number += 1
 
-        print("Cards in the trick: ", trick.cards)
         print(f"Player {winner.name} wins the trick.")
         print()
 
     return
 
 
-###############
-## Utilities ##
-###############
+#################
+## Subroutines ##
+#################
+
+
+def _play_trick(players: list[Player], leader: Player) -> Player:
+    """Plays a single trick."""
+    num_players = len(players)
+
+    # -- Determine play order -- #
+    start = players.index(leader)
+    play_order = [players[(start + i) % num_players] for i in range(num_players)]
+
+    # -- Play the trick -- #
+    trick = Pile()
+
+    for current_player in play_order:
+        card = current_player.play()
+        print(f"{current_player.name} plays: {card}")
+        trick.add(card)
+
+    # -- Determine the winner -- #
+    winner = play_order[trick.cards.index(winning_card(trick))]
+    winner.tricks.append(trick)
+
+    return winner
