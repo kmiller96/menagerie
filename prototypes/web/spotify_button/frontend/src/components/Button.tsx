de544ae596fa725df37af0c5a1f583653c5e6e1b
Kale Miller <kale.miller@prometheusai.com.au>
2024-08-04 13:55:07 +0800

    Removed unneeded logging

--- prototypes/web/spotify_button/frontend/src/components/Button.tsx
+++ prototypes/web/spotify_button/frontend/src/components/Button.tsx
@@ -20,8 +20,6 @@ export function Button({
       onClick={async () => {
         const response = await fetch(`http://localhost:8000/song/${songLink}`);
 
-        console.log(JSON.stringify(response));
-
         if (!response.ok) {
           throw new Error(
             "Failed to play song.\nReason: " + response.statusText
