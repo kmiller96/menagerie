ce508436959d62fa05d964a3985bef02d9cd0538
Kale Miller <kale.miller@prometheusai.com.au>
2024-08-22 09:50:44 +0800

    Created a simple python package to help clean up code

---
+++ prototypes/presentations/script_to_system/utils/README.md
@@ -0,0 +1,4 @@
+# Utilities
+
+Simple python package full of script utilities. Helps keep the main codebase
+focused.
---
+++ prototypes/presentations/script_to_system/utils/src/utils/database.py
@@ -0,0 +1,20 @@
+"""Contains the database utility class."""
+
+from pathlib import Path
+
+
+class Database:
+    """Simulates a simple database. We do this with a plain text CSV file."""
+
+    def __init__(self, here: str):
+        """Initializes the database.
+
+        Args:
+            here (str): The `__file__` of the script.
+        """
+        self.path = Path(here).parent / "database.db"
+
+    def insert(self, data: list[int]):
+        """Appends data to the database."""
+        with open(self.path, "a") as file:
+            file.write(",".join(data) + "\n")
