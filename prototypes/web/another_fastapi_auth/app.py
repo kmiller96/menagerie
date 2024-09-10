0bf6cdc06bff8f556a737f5db25abba5cab9e243
Kale Miller <kale.miller@prometheusai.com.au>
2024-09-10 10:14:12 +0800

    Setup dummy login/logout endpoints

--- prototypes/web/another_fastapi_auth/app.py
+++ prototypes/web/another_fastapi_auth/app.py
@@ -1,7 +1,12 @@
 import fastapi
+from pydantic import BaseModel
 
 
 app = fastapi.FastAPI()
+
+##############
+## Standard ##
+##############
 
 
 @app.get("/")
@@ -12,3 +17,34 @@ def unprotected():
 @app.get("/protected")
 def protected():
     return {"message": "Protected route"}
+
+
+##########
+## Auth ##
+##########
+
+
+class User(BaseModel):
+    username: str
+    password: str  # NOTE: This is a hash in a real application
+
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login")
+def login(req: LoginRequest, response: fastapi.Response):
+    if req.username == "test" and req.password == "test":
+        response.status_code = 200
+        return {"message": "Login successful"}
+
+    else:
+        response.status_code = 401
+        return {"message": "Login failed"}
+
+
+@app.post("/logout")
+def logout():
+    return {"message": "Logout successful"}
