69e3c575f7d101c8cb06a0fc0b6a4ebf9b14e19e
Kale Miller <kale.miller@prometheusai.com.au>
2024-09-24 20:32:16 +0800

    Setup first jinja template

--- prototypes/web/twitter_clone/main.py
+++ prototypes/web/twitter_clone/main.py
@@ -1,9 +1,17 @@
 import fastapi
 
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 app = fastapi.FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def healthcheck():
-    return "ok"
+
+@app.get("/", response_class=HTMLResponse)
+def healthcheck(request: fastapi.Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.jinja",
+    )
