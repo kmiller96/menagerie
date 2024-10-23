1f15cfbca20e1119e2856d801bb190c0949dc465
Kale Miller <kale.miller@prometheusai.com.au>
2024-10-23 11:57:02 +0800

    Added a super simple UI

--- prototypes/web/react_google_sso/src/App.tsx
+++ prototypes/web/react_google_sso/src/App.tsx
@@ -1,7 +1,49 @@
 export function App() {
+  console.log(import.meta.env);
+
   return (
-    <>
-      <p>Hello world!</p>
-    </>
+    <div
+      id="container"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "lightblue",
+      }}
+    >
+      <div
+        id="login-container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 10,
+          backgroundColor: "white",
+        }}
+      >
+        <StyledButton onClick={() => alert("Login")}>Login</StyledButton>
+        <StyledButton onClick={() => alert("Logout")}>Logout</StyledButton>
+      </div>
+    </div>
+  );
+}
+
+function StyledButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      style={{
+        fontSize: 18,
+        marginTop: 5,
+        marginBottom: 5,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: "8px",
+        backgroundColor: "white",
+        boxShadow: "3px 3px rgba(0,0,0,0.2)",
+        ...props,
+      }}
+      {...props}
+    />
   );
 }
