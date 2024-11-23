3ab1ada2dfa7574ce8e21a0d8e6b5534026320d0
Kale Miller <kale.miller@prometheusai.com.au>
2024-11-23 20:56:28 +0800

    Replaced manual map with a for loop component

--- prototypes/ui/item_list_with_sidebar/src/components/ItemList.tsx
+++ prototypes/ui/item_list_with_sidebar/src/components/ItemList.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+import { VStack, For } from "@chakra-ui/react";
 
 import { Item } from "@/types";
 
@@ -8,9 +8,7 @@ import { ListItem } from "./ListItem";
 export function ItemList({ items }: { items: Item[] }) {
   return (
     <VStack align="left" gap={4}>
-      {items.map((item) => (
-        <ListItem key={item.id} item={item} />
-      ))}
+      <For each={items}>{(item) => <ListItem key={item.id} item={item} />}</For>
     </VStack>
   );
 }
