653c2d53904d8d3aff8cd1332b2764ab34810d78
Kale Miller <kale.miller@prometheusai.com.au>
2024-12-28 11:40:16 +0800

    Setup a simple 'hello world' for the terminal app

--- prototypes/rust/tui/ratatui/simple_note_app/src/main.rs
+++ prototypes/rust/tui/ratatui/simple_note_app/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::thread;
+use std::time;
+
+use ratatui;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+
+    terminal.clear()?;
+    terminal.draw(|frame| {
+        frame.render_widget("Hello World!", frame.area());
+    })?;
+
+    thread::sleep(time::Duration::from_secs(2));
+
+    ratatui::restore();
+    Ok(())
 }
