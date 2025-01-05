70a5c7611a2bb60a4032c2a9cb4286c3300b1258
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-05 10:18:11 +0800

    Printed coordinates of grid?

--- prototypes/graphics/hexgrid/index.js
+++ prototypes/graphics/hexgrid/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { defineHex, Grid, rectangle } from 'honeycomb-grid'
+
+// 1. Create a hex class:
+const Tile = defineHex({ dimensions: 30 })
+
+// 2. Create a grid by passing the class and a "traverser" for a rectangular-shaped grid:
+const grid = new Grid(Tile, rectangle({ width: 10, height: 10 }))
+
+// 3. Iterate over the grid to log each hex:
+grid.forEach(console.log)
