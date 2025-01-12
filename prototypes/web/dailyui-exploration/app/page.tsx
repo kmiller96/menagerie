95cdc0213aa1e52f1aa24af102565133336ca3c6
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-12 10:48:40 +0800

    Okay I'm done fiddling

--- prototypes/web/dailyui-exploration/app/page.tsx
+++ prototypes/web/dailyui-exploration/app/page.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "@/components/themeToggle";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-min-screen">
       <div className="flex flex-col w-6/12 gap-5 justify-center align-middle">
         <ThemeToggle />
         <Modal />
