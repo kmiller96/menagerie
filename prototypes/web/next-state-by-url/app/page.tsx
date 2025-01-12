b3e56972caf8f81c89e681200048c25f4397cf3b
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-12 16:38:18 +0800

    Cleaned up the page

--- prototypes/web/next-state-by-url/app/page.tsx
+++ prototypes/web/next-state-by-url/app/page.tsx
@@ -1,25 +1,37 @@
+function Title() {
+  return (
+    <div className="mb-5">
+      <h1 className="text-5xl">Product Search</h1>
+      <h2 className="italic">What product would you like to access?</h2>
+    </div>
+  );
+}
+
+function SearchForm() {
+  return (
+    <div className="pb-[200px] w-full">
+      <form action="/search" method="GET">
+        <div className="flex flex-row join">
+          <input
+            type="text"
+            placeholder="What are you looking for?"
+            className="input input-bordered flex-grow join-item"
+          />
+          <button className="btn btn-primary join-item" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-grow items-center justify-center h-min-screen">
       <div className="w-6/12 flex flex-col items-center justify-center">
-        <div className="mb-5">
-          <h1 className="text-5xl">Product Search</h1>
-          <h2 className="italic">What product would you like to access?</h2>
-        </div>
-        <div className="pb-[200px] w-full">
-          <form action="/search" method="GET">
-            <div className="flex flex-row join">
-              <input
-                type="text"
-                placeholder="What are you looking for?"
-                className="input input-bordered flex-grow join-item"
-              />
-              <button className="btn btn-primary join-item" type="submit">
-                Search
-              </button>
-            </div>
-          </form>
-        </div>
+        <Title />
+        <SearchForm />
       </div>
     </div>
   );
