5d04c8abe364db32d479aceda2918d0087151d3d
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-12 10:09:47 +0800

    Align stuff to the center of the page

--- prototypes/web/dailyui-exploration/app/page.tsx
+++ prototypes/web/dailyui-exploration/app/page.tsx
@@ -2,11 +2,13 @@ import { ThemeToggle } from "@/components/themeToggle";
 
 export default function Home() {
   return (
-    <div>
-      <ThemeToggle />
-      <button className="btn btn-primary">One</button>
-      <button className="btn btn-secondary">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col w-6/12 justify-center align-middle">
+        <ThemeToggle />
+        <button className="btn btn-primary">One</button>
+        <button className="btn btn-secondary">Two</button>
+        <button className="btn btn-accent btn-outline">Three</button>
+      </div>
     </div>
   );
 }
