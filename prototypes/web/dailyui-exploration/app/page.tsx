b620318f7178903a861c0dae01501ce75c03ead6
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-12 09:37:29 +0800

    Setup a theme toggler

--- prototypes/web/dailyui-exploration/app/page.tsx
+++ prototypes/web/dailyui-exploration/app/page.tsx
@@ -1,6 +1,9 @@
+import { ThemeToggle } from "@/components/themeToggle";
+
 export default function Home() {
   return (
     <div>
+      <ThemeToggle />
       <button className="btn btn-primary">One</button>
       <button className="btn btn-secondary">Two</button>
       <button className="btn btn-accent btn-outline">Three</button>
