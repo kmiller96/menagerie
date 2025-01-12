47ead0d1b354a59eed23433751979aec81cf381d
Kale Miller <kale.miller@prometheusai.com.au>
2025-01-12 10:20:28 +0800

    Committed messy head

--- prototypes/games/hexgrid/src/main.ts
+++ prototypes/games/hexgrid/src/main.ts
@@ -1,4 +1,7 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets, Graphics, Sprite } from "pixi.js";
+
+import { defineHex, Grid, rectangle } from "honeycomb-grid";
+
 import { square, hexagon } from "./shapes";
 
 async function initApp(): Promise<Application> {
@@ -12,29 +15,34 @@ async function initApp(): Promise<Application> {
 
 (async () => {
   const app = await initApp();
+  const graphics = new Graphics();
 
-  const length = 100;
-  const hexWidth = 2 * length;
-  const hexHeight = Math.sqrt(3) * length;
+  const Hex = defineHex({ dimensions: 30 });
+  const grid = new Grid(Hex, rectangle({ width: 10, height: 10 }));
 
-  const horizontalSpacing = (3 / 4) * hexWidth;
-  const verticalSpacing = hexHeight;
+  const renderHex = (hex: Hex) => {
 
-  const x = 100;
-  const y = 100;
+  grid.forEach(renderHex)
 
-  // Starting hex
-  app.stage.addChild(hexagon({ x, y, length }));
-
-  app.stage.addChild(hexagon({ x, y: y + verticalSpacing / 2, length }));
-  app.stage.addChild(hexagon({ x, y: y + (2 * verticalSpacing) / 2, length }));
-
-  app.stage.addChild(
-    hexagon({
-      x: x + horizontalSpacing / 2,
-      y: y + verticalSpacing / 4,
-      length,
-    })
-  );
-  app.stage.addChild(hexagon({ x, y: y + (2 * verticalSpacing) / 2, length }));
 })();
+
+import * as PIXI from "pixi.js";
+
+// you may want the origin to be the top left corner of a hex's bounding box
+// instead of its center (which is the default)
+const Hex = defineHex({ dimensions: 30, origin: "topLeft" });
+const grid = new Grid(Hex, rectangle({ width: 10, height: 10 }));
+
+const app = new PIXI.Application({ backgroundAlpha: 0 });
+const graphics = new PIXI.Graphics();
+
+document.body.appendChild(app.view);
+graphics.lineStyle(1, 0x999999);
+
+grid.forEach(renderHex);
+app.stage.addChild(graphics);
+
+function renderHex(hex: Hex) {
+  // PIXI.Polygon happens to be compatible with hex.corners
+  graphics.drawShape(new PIXI.Polygon(hex.corners));
+}
