9417893201279a56e6943496bcdbe419b590ecbe
Kale Miller <kale.miller@prometheusai.com.au>
2025-03-01 10:16:14 +0800

    Tidied up the pixijs codebase

---
+++ prototypes/pixijs/movearound/README.md
@@ -0,0 +1,10 @@
+# Movearound
+
+Little prototype exploring how to move a sprite around the page.
+
+## Quickstart
+
+```bash
+pnpm install
+pnpm dev
+```
--- prototypes/pixijs/movearound/src/main.ts
+++ prototypes/pixijs/movearound/src/main.ts
@@ -1,35 +1,26 @@
 import { Application, Assets, Sprite } from "pixi.js";
 
 (async () => {
-  // Create a new application
+  // Create application
   const app = new Application();
-
-  // Initialize the application
   await app.init({ background: "#1099bb", resizeTo: window });
 
-  // Append the application canvas to the document body
+  // Add to document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-  // Load the bunny texture
+  // Load bunny
   const texture = await Assets.load("/assets/bunny.png");
-
-  // Create a bunny Sprite
   const bunny = new Sprite(texture);
 
-  // Center the sprite's anchor point
+  // Position bunny
   bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
   bunny.position.set(app.screen.width / 2, app.screen.height / 2);
 
   // Add the bunny to the stage
   app.stage.addChild(bunny);
 
-  // Listen for animate update
+  // Animate
   app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
     bunny.rotation += 0.1 * time.deltaTime;
   });
 })();
