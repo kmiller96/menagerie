60e45e034d5e0f0e05c657a19debe9eb58342874
Kale Miller <kale.miller@prometheusai.com.au>
2025-03-01 10:21:35 +0800

    Made the bunny move around!

--- prototypes/pixijs/movearound/src/main.ts
+++ prototypes/pixijs/movearound/src/main.ts
@@ -1,5 +1,7 @@
 import { Application, Assets, Sprite } from "pixi.js";
 
+type Velocity = { x: number; y: number };
+
 (async () => {
   // Create application
   const app = new Application();
@@ -19,8 +21,41 @@ import { Application, Assets, Sprite } from "pixi.js";
   // Add the bunny to the stage
   app.stage.addChild(bunny);
 
+  // Add event listeners
+  const v = { x: 0, y: 0 } as Velocity;
+  window.onkeydown = (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+        v.y = -10;
+        break;
+      case "ArrowDown":
+        v.y = +10;
+        break;
+      case "ArrowLeft":
+        v.x = -10;
+        break;
+      case "ArrowRight":
+        v.x = +10;
+        break;
+    }
+  };
+
+  window.onkeyup = (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+      case "ArrowDown":
+        v.y = 0;
+        break;
+      case "ArrowLeft":
+      case "ArrowRight":
+        v.x = 0;
+        break;
+    }
+  };
+
   // Animate
   app.ticker.add((time) => {
-    bunny.rotation += 0.1 * time.deltaTime;
+    bunny.position.x += v.x;
+    bunny.position.y += v.y;
   });
 })();
