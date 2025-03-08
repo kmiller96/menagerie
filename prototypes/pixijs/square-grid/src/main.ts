617118f88b3ae6d2bc6ef9c28d1db469eb6bc425
Kale Miller <kale.miller@prometheusai.com.au>
2025-03-08 10:40:44 +0800

    Added a simple pixel painter program

--- prototypes/pixijs/square-grid/src/main.ts
+++ prototypes/pixijs/square-grid/src/main.ts
@@ -1,35 +1,76 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Graphics, GraphicsContext } from "pixi.js";
 
-(async () => {
-  // Create a new application
+const BACKGROUND_COLOR = 0xffffff;
+
+let ISLEFTHOLDING = false;
+let ISRIGHTHOLDING = false;
+
+async function init(): Promise<Application> {
   const app = new Application();
+  await app.init({ background: BACKGROUND_COLOR, resizeTo: window });
 
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
-
-  // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  return app;
+}
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+async function registerPointerEvents() {
+  window.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    if (e.button === 0) ISLEFTHOLDING = true;
+    if (e.button === 2) ISRIGHTHOLDING = true;
   });
+
+  window.addEventListener("mouseup", (e) => {
+    e.preventDefault();
+    if (e.button === 0) ISLEFTHOLDING = false;
+    if (e.button === 2) ISRIGHTHOLDING = false;
+  });
+}
+
+/** Creates the grid graphic */
+async function drawGrid(
+  app: Application,
+  { gridSize = 50 }: { gridSize?: number }
+) {
+  // Determine grid geometry
+  const nRows = Math.floor(app.screen.height / gridSize);
+  const nCols = Math.floor(app.screen.width / gridSize);
+
+  const xPadding = (app.screen.width - nCols * gridSize) / 2;
+  const yPadding = (app.screen.height - nRows * gridSize) / 2;
+
+  // Draw graphics context
+  const squareContext = new GraphicsContext()
+    .rect(0, 0, gridSize, gridSize)
+    .fill(BACKGROUND_COLOR)
+    .stroke(0x000000);
+
+  // Draw grid
+  for (let i = 0; i < nRows; i++) {
+    for (let j = 0; j < nCols; j++) {
+      const square = new Graphics(squareContext);
+
+      square.position.set(j * gridSize + xPadding, i * gridSize + yPadding);
+
+      const paintSquare = () => {
+        if (ISLEFTHOLDING) square.tint = 0x0000ff;
+        if (ISRIGHTHOLDING) square.tint = 0xff0000;
+      };
+
+      square.eventMode = "static";
+
+      square.on("mousedown", paintSquare);
+      square.on("pointerenter", paintSquare);
+
+      app.stage.addChild(square);
+    }
+  }
+}
+
+(async () => {
+  const app = await init();
+
+  await registerPointerEvents();
+  await drawGrid(app, { gridSize: 70 });
 })();
