36b67c00054e4b9dc72e93168b8f7d84cc4fae20
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-11 22:16:13 -0400

    Setup the rocket server

--- prototypes/rust/new/string_functions/src/main.rs
+++ prototypes/rust/new/string_functions/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn health_check() -> &'static str {
+    "{\"status\": \"OK\"}"
+}
+
+#[get("/test")]
+fn test() -> &'static str {
+    "{\"status\": \"OK\"}"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![health_check, test])
 }
---
+++ prototypes/rust/new/string_functions/test.sh
@@ -0,0 +1,4 @@
+BASE_URL=http://localhost:8000
+
+curl $BASE_URL/
+curl $BASE_URL/test
