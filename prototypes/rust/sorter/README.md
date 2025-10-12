14fbe731f46e44a1a6ffb0447243f8a1b77f3a1e
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-11 22:56:10 -0400

    Setup scaffolding of the new program

--- prototypes/rust/sorter/README.md
+++ prototypes/rust/sorter/README.md
@@ -4,3 +4,9 @@ CLI program to prefix files and folders with a timestamp.
 
 > I had the issue of wanting to order my projects in created order. So I thought
 > that would be a great project to build!
+
+Usage:
+
+```bash
+sorter <path> --depth 1 --dry-run
+```
--- prototypes/rust/sorter/src/main.rs
+++ prototypes/rust/sorter/src/main.rs
@@ -1,3 +1,50 @@
+// ----------- //
+// -- Types -- //
+// ----------- //
+
+type Path = String;
+type PathVec = Vec<Path>;
+
+// ----------------- //
+// -- Subroutines -- //
+// ----------------- //
+
+/// Loads the CLI arguments.
+fn parse_cli() {
+    // Read CLI with clap
+}
+
+const TEST_PATH: &str = "path/to/file";
+
+/// Discover files in the given path up to the specified depth
+fn find(depth: u32) -> PathVec {
+    vec![TEST_PATH.to_string()]
+}
+
+/// Sorts the given paths based on metadata.
+fn sort(paths: &PathVec) -> PathVec {
+    paths.clone()
+}
+
+/// Migrates files from the old location to the new location.
+fn migrate(old: &Path, new: &Path) {
+    return ();
+}
+
+// ------------------ //
+// -- Main Routine -- //
+// ------------------ //
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_cli();
+    let paths = find(3);
+    let targets = sort(&paths);
+
+    // TODO: Perform move command on each path
+    for i in 0..(targets.clone().len()) {
+        let p = &paths[i];
+        let t = &targets[i];
+
+        migrate(p, t);
+    }
 }
