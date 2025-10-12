3e8274fe643fbab6d8794883c43437c1fae53b0a
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-11 22:21:35 -0400

    Boilerplate for the future string functions

--- prototypes/rust/new/string_functions/src/main.rs
+++ prototypes/rust/new/string_functions/src/main.rs
@@ -1,17 +1,44 @@
 #[macro_use]
 extern crate rocket;
 
+// ------------ //
+// -- Routes -- //
+// ------------ //
+
+/// Simple healthcheck to verify the server is running
 #[get("/")]
 fn health_check() -> &'static str {
     "{\"status\": \"OK\"}"
 }
 
-#[get("/test")]
-fn test() -> &'static str {
-    "{\"status\": \"OK\"}"
+/// Reverses the input string
+#[get("/reverse")]
+fn reverse() {
+    // stub
 }
+
+/// Converts the input string to uppercase
+#[get("/upper")]
+fn upper() {
+    // stub
+}
+
+/// Converts the input string to lowercase
+#[get("/lower")]
+fn lower() {
+    // stub
+}
+
+#[get("/test")]
+fn stub() -> &'static str {
+    "stubby :("
+}
+
+// ------------ //
+// -- Launch -- //
+// ------------ //
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![health_check, test])
+    rocket::build().mount("/", routes![health_check, reverse, upper, lower, stub])
 }
