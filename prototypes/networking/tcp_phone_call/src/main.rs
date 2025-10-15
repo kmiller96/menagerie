e52a383cf3f12001d69713862a8206caee300e56
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-14 21:07:42 -0400

    Installed the CLI logic

--- prototypes/networking/tcp_phone_call/src/main.rs
+++ prototypes/networking/tcp_phone_call/src/main.rs
@@ -1,22 +1,66 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+// -------------------- //
+// -- CLI Definition -- //
+// -------------------- //
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+/// TCP Calling Software
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CLI {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Subcommand)]
+enum Commands {
+    /// Start the TCP phone call server
+    Server {
+        /// IP address to bind the server to
+        #[arg(short, long, default_value = "127.0.0.1")]
+        ip: String,
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+        /// Port to bind the server to
+        #[arg(short, long, default_value_t = 8000)]
+        port: u16,
+    },
+
+    /// Start a TCP phone call client
+    Client {
+        /// Server IP address to connect to
+        #[arg(short, long, default_value = "127.0.0.1")]
+        ip: String,
+
+        /// Server port to connect to
+        #[arg(short, long, default_value_t = 8000)]
+        port: u16,
+    },
+}
+
+// ----------------- //
+// -- Subroutines -- //
+// ----------------- //
+
+fn run_server(ip: String, port: u16) {
+    println!("Starting server at {}:{}", ip, port);
+}
+fn run_client(ip: String, port: u16) {
+    println!("Starting client to connect to {}:{}", ip, port);
+}
+
+// ------------------ //
+// -- Main Routine -- //
+// ------------------ //
+
+fn main() {
+    let args = CLI::parse();
+
+    match args.command {
+        Commands::Server { ip, port } => {
+            run_server(ip, port);
+        }
+        Commands::Client { ip, port } => {
+            run_client(ip, port);
+        }
     }
 }
