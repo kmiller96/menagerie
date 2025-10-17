749f7b13d624633057cbcec27eeb7f849007388d
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-17 11:34:13 -0400

    Updated the example with my own threading logic

--- prototypes/rust/rust_book_threading/src/main.rs
+++ prototypes/rust/rust_book_threading/src/main.rs
@@ -1,16 +1,30 @@
 use std::thread;
 use std::time::Duration;
 
-fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+const N_THREADS: usize = 10;
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
+fn main() {
+    // Create thread collection
+    let mut threads = vec![];
+
+    // Create slow threads
+    for i in 1..=N_THREADS {
+        threads.push(thread::spawn(move || {
+            thread::sleep(Duration::from_millis((N_THREADS - i) as u64 * 200));
+            println!("Thread {i} done!");
+        }));
     }
+
+    // Quickly print from the main thread
+    for i in 1..=5 {
+        println!("hi number {i} from the main thread!");
+    }
+
+    // Join all the threads
+    for handle in threads {
+        handle.join().unwrap();
+    }
+
+    // Print a final completion message
+    println!("All threads completed!");
 }
