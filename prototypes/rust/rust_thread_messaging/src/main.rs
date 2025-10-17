39e6f3c8329d88a6d39cbf1b6a0c49a0228cb174
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-17 12:02:32 -0400

    Played around with sending from multiple upstream threads

--- prototypes/rust/rust_thread_messaging/src/main.rs
+++ prototypes/rust/rust_thread_messaging/src/main.rs
@@ -3,26 +3,44 @@ use std::thread;
 use std::time::Duration;
 
 #[derive(Debug, Clone)]
-struct Message;
+struct Message {
+    content: String,
+}
 
 impl Message {
-    fn new() -> Self {
-        Message {}
+    pub fn new(content: String) -> Self {
+        Message { content }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
     }
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel::<Message>();
 
-    println!("Spawning thread to send a message...");
+    println!("Spawning thread #1 to send a message...");
+    let tx1 = tx.clone();
     thread::spawn(move || {
         thread::sleep(Duration::from_secs(1));
 
-        let message = Message::new();
-        tx.send(message.clone()).unwrap();
-        println!("Message sent: {:?}", message);
+        let message = Message::new("Hello from thread #1".to_string());
+        tx1.send(message).unwrap();
+        println!("Message sent from thread #1");
+    });
+
+    println!("Spawning thread #2 to send a message...");
+    let tx2 = tx.clone();
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(2));
+
+        let message = Message::new("Hello from thread #2".to_string());
+        tx2.send(message).unwrap();
+        println!("Message sent from thread #2");
     });
 
     println!("Waiting for message...");
-    println!("Message received: {:?}", rx.recv().unwrap());
+    println!("Message received: {:?}", rx.recv().unwrap().content());
+    println!("Message received: {:?}", rx.recv().unwrap().content());
 }
