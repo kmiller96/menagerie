09c511a9146176b33aa3be61c4bacf56afe67e18
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-17 11:52:30 -0400

    Create a simple messaging thread

--- prototypes/rust/rust_thread_messaging/src/main.rs
+++ prototypes/rust/rust_thread_messaging/src/main.rs
@@ -1,3 +1,27 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug)]
+struct Message;
+
+impl Message {
+    fn new() -> Self {
+        Message {}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel::<Message>();
+
+    println!("Spawning thread to send a message...");
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(1));
+
+        let message = Message::new();
+        tx.send(message).unwrap();
+    });
+
+    println!("Waiting for message...");
+    println!("Message received: {:?}", rx.recv().unwrap());
 }
