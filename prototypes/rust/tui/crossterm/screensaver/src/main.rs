f6e0a71518b5a1fb91c8a74fc8a7851198f91756
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-26 23:02:00 -0400

    Wrote my own crossterm code

--- prototypes/rust/tui/crossterm/screensaver/src/main.rs
+++ prototypes/rust/tui/crossterm/screensaver/src/main.rs
@@ -1,25 +1,73 @@
-use crossterm::{
-    cursor,
-    style::{self, Stylize},
-    terminal, ExecutableCommand, QueueableCommand,
+use std::{
+    io::{self, Write},
+    thread, time,
 };
-use std::io::{self, Write};
+
+use crossterm::{cursor, execute, queue, style, terminal};
+
+const FPS: u128 = 5;
+const TICK_DURATION: u128 = 1000 / FPS;
+
+// ------------------ //
+// -- Main Routine -- //
+// ------------------ //
 
 fn main() -> io::Result<()> {
-    let mut stdout = io::stdout();
+    // TEMP: for testing only
+    let mut counter = 0;
 
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    // Setup stdout
+    let mut stdout = initialise()?;
 
-    for y in 0..40 {
-        for x in 0..150 {
-            if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-                // in this loop we are more efficient by not flushing the buffer.
-                stdout
-                    .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".magenta()))?;
-            }
+    // Run main loop
+    loop {
+        // Start loop timer
+        let start = time::SystemTime::now();
+
+        // Queue up drawing commands
+        queue!(
+            stdout,
+            terminal::Clear(terminal::ClearType::All),
+            cursor::MoveTo(0, 0),
+            style::Print(format!("Counter: {}\n", counter))
+        )?;
+
+        // Flush all queued commands
+        stdout.flush()?;
+
+        // Wait as long as required to stick to desired FPS
+        let elapsed = start.elapsed().unwrap().as_millis();
+
+        if elapsed < TICK_DURATION {
+            let delta = TICK_DURATION - elapsed;
+            let duration = time::Duration::from_millis(delta as u64);
+            thread::sleep(duration);
+        }
+
+        // TEMP: For testing
+        if counter >= 100 {
+            break;
+        } else {
+            counter += 1;
         }
     }
-    stdout.flush()?;
+
     Ok(())
+}
+
+// --------------- //
+// -- Utilities -- //
+// --------------- //
+
+/// Initialises the STDOUT to be writable for crossterm.
+fn initialise() -> io::Result<io::Stdout> {
+    let mut stdout = io::stdout();
+
+    execute!(
+        stdout,
+        crossterm::terminal::EnterAlternateScreen,
+        style::SetForegroundColor(style::Color::Green)
+    )?;
+
+    Ok(stdout)
 }
