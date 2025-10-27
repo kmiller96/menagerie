5fcd21168111b2d32a1549144a2a69e935dfd142
Kale Miller <kale.miller@prometheusai.com.au>
2025-10-26 22:37:23 -0400

    Installed the example code from crossterm

--- prototypes/rust/tui/crossterm/screensaver/src/main.rs
+++ prototypes/rust/tui/crossterm/screensaver/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor,
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    for y in 0..40 {
+        for x in 0..150 {
+            if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+                // in this loop we are more efficient by not flushing the buffer.
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::PrintStyledContent("█".magenta()))?;
+            }
+        }
+    }
+    stdout.flush()?;
+    Ok(())
 }
